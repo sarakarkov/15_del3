0ee1ffb7ab3f266f26ea5981c6ced1ce4c64f91b
Mina Ahmadi <s195381@student.dtu.dk>
2019-11-26 15:50:38 +0100

    Ændring af "M" til dollartegn på chancekort Tiføjelse af JUnit test

--- src/main/java/Model/Field/ChanceField.java
+++ src/main/java/Model/Field/ChanceField.java
@@ -25,18 +25,18 @@ public class ChanceField extends Field {
 
     public void initChanceCards(){
         chanceCards = new ChanceCard[]{
-            new MoveToCard("Chance1# Ryk frem til start og modtag M2", 24),
+            new MoveToCard("Chance1# Ryk frem til start og modtag $1", 24),
             new MoveForwardCard("Chance2# Ryk 5 felter frem", 5),
             new FreeCard("Chance3# Gratis felt. Ryk frem til et vilkårligt orange felt. Hvis det er ledigt får du det gratis ellers skal du betale leje til ejeren", new int[]{10, 11}),
             new MoveForwardCard("Chance4# Ryk et felt frem og tag et chancekort mere", 1),
-            new MoneyCard("Chance5# Du har spist for meget slik betal M2 til banken", -2),
+            new MoneyCard("Chance5# Du har spist for meget slik betal $2 til banken", -2),
             new FreeCard("Chance6# Gratis felt. Ryk frem til et vilkårligt grønt felt. Hvis det er ledig får du det gratis, ellers skal du betale leje til ejeren", new int[]{19, 20}),
             new FreeCard("Chance7# Gratis felt. Ryk frem til et vilkårligt lyse blåt felt. Hvis det er ledig får du det gratis, ellers skal du betale leje til ejeren", new int[]{4, 5}),
             new JailCard("Chance8# Du løslades uden omkostninger. Behold dette kort indtil du får brug for det"),
             new MoveToCard("Chance9# Ryk til Strandpromenaden", 23),
-            new MoneyCard("Chance10# Det er din fødselsdag. Alle giver dig M1. Tillykke med fødselsdagen", 1),
+            new MoneyCard("Chance10# Det er din fødselsdag. Alle giver dig $1. Tillykke med fødselsdagen", 1),
             new FreeCard("Chance11# Gratis felt. Ryk frem til et vilkårligt pink eller vilkårligt mørkeblåt felt. Hvis det er ledigt får du det gratis, ellers skal du betale leje til ejeren", new int[]{7,8,22,23}),
-            new MoneyCard("Chance12# Du har lavet alle dine lektier. Modtag M3 fra banken", 3),
+            new MoneyCard("Chance12# Du har lavet alle dine lektier. Modtag $3 fra banken", 3),
             new FreeCard("Chance13# Gratis felt. Ryk frem til et vilkårligt rødt felt. Hvis det er ledig får du det gratis, ellers skal du betale leje til ejeren", new int[]{13,14}),
             new FreeCard("Chance14# Gratis felt. Ryk frem til Skaterparken for at lave det perfekte grind! Hvis ingen ejer det, får du den gratis ellers skal du betale leje til ejeren", new int[]{10})
         };
---
+++ src/test/java/Model/Field/StreetFieldTest.java
@@ -0,0 +1,23 @@
+package Model.Field;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+// Oprettet test på en metode i klassen "StreetFieldTest"
+// Erklærer testobjektet "Iskiosken" som en global variabel
+// Kalder metoden på testobjektet som det ene parameter, og det forventede resultat som det andet parameter
+// Testen er bestået
+
+public class StreetFieldTest {
+
+    StreetField Iskiosken = new StreetField("Iskiosken", "$1", "Bli kølet ned", 1, new Color(135, 210, 255));
+
+    @Test
+    public void getRent() {
+
+        assertEquals(Iskiosken.getRent(), 1);
+    }
+}
